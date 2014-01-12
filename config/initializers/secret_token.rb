3cbfa894a533f917e62404aace5ea3be95d08c52
training <training@localhost.localdomain>
2014-01-12 04:51:39 -0800

    Create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KikuponApi::Application.config.secret_key_base = 'a9dd8f7dc595508b3b43a5e4b8e39751ddf5562ad873c5f3defbd63f67d86b39ea08eb046b7dc9e09c01194175a3ab49f6543e0951ae1552f891f8cc4fe88bf8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KikuponApi::Application.config.session_store :cookie_store, key: '_kikupon_api_session'
